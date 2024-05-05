d072c2d4eb7ce3ce6375cb6bd7c33174f2cad1df
marmakoide <alexandre.devert@algokhod.com>
2024-05-05 22:56:47 +0200

    Temporary build files are built into a separate build directory

--- firmware/config.mk
+++ firmware/config.mk
@@ -1,2 +1,8 @@
 AVRKIT_PATH=/home/alex/Labo/embedded/arduino/avrkit
 USB_PORT=/dev/ttyUSB0
+
+MCU=atmega328p
+F_CPU=16000000UL
+
+CC=avr-gcc 
+CFLAGS=-Os -DF_CPU=$(F_CPU) -D__AVRKIT_$(MCU)__ -mmcu=$(MCU)
